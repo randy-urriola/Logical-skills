7092664bd54ac9c1be8e9f5acc15b0b62e97a96d
Randy Urriola <randy.urriolah@gmail.com>
2023-04-18 15:21:21 -0500

    merge two link list

--- mergeLists.js
+++ mergeLists.js
@@ -16,28 +16,16 @@ function merge(list1, list2) {
 	let nodo = temp;
 
 	while (list1 && list2) {
-		if (list1.val <= list2.val) {
+		if (list1.val < list2.val) {
 			nodo.next = list1;
-			nodo = list1;
 			list1 = list1.next;
 		} else {
 			nodo.next = list2;
-			nodo = list2;
 			list2 = list2.next;
 		}
+		nodo = nodo.next;
 	}
 
-	while (list1) {
-		nodo.next = list1;
-		nodo = list1;
-		list1 = list1.next;
-	}
-	while (list2) {
-		nodo.next = list2;
-		nodo = list2;
-		list2 = list2.next;
-	}
+	nodo.next = list1 || list2;
 	return temp.next;
 }
-
-console.log(merge([1, 2, 4], [1, 3, 4]));
